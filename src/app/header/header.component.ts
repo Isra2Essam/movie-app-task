20d4a3ada04032ead0d132c40d58bfd65da18832
Isra2Essam <israaessam12300@gmail.com>
2021-04-09 19:56:26 +0200

    Setup header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user.subscribe((user) => {
+      this.isLoggedIn = !user ? false : true;
+    });
+  }
+}
