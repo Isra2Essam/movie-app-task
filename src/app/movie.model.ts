0f9ddac2b8c54af4c55050312b4f99d12396ab3b
Isra2Essam <israaessam12300@gmail.com>
2021-04-09 21:34:12 +0200

    Enhance model

--- src/app/movie.model.ts
+++ src/app/movie.model.ts
@@ -1,3 +1,3 @@
 export class Movie {
-    constructor(public base_url: string,public size: string,public poster_path: string, public original_title: string, public overview: string, public popularity: string, public release_date: string,public id:number) { }
+    constructor(public base_url: string,public size: string,public poster_path: string, public original_title: string, public overview: string, public id:number) { }
 }
